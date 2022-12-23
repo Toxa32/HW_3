601a43a12e547860d6a2432ac1bbd6d7edd5480e
anton <gumen.tosha@yandex.ru>
2022-12-23 12:48:56 +0300

    DZ3

---
+++ task_21/Program.cs
@@ -0,0 +1,22 @@
+﻿int aX, aY, aZ;
+int bX, bY, bZ;
+
+Console.WriteLine("Введите посследовательность координат: ");
+Console.Write("aX: ");
+aX = Convert.ToInt32(Console.ReadLine());
+Console.Write("aY: ");
+aY = Convert.ToInt32(Console.ReadLine());
+Console.Write("aZ: ");
+aZ = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("bX: ");
+bX = Convert.ToInt32(Console.ReadLine());
+Console.Write("bY: ");
+bY = Convert.ToInt32(Console.ReadLine());
+Console.Write("bZ: ");
+bZ = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Pow(bX - aX, 2) + Math.Pow(bY - aY, 2) + Math.Pow(bZ - aZ, 2);
+result = Math.Sqrt(result);
+result = Math.Round(result, 2);
+Console.WriteLine(result);
